ba016c2a1a99f52e2046a775b2e351618ec294d7
vexus <cdolson30@gmail.com>
2019-11-16 00:00:14 -0500

    Fixed issue with overlapping error embed messages.

--- cmd/eval.js
+++ cmd/eval.js
@@ -1,23 +1,32 @@
 const error = require("../lib/error");
-const KIOID = "114843644896346116";
+const Discord = require("discord.js")
+
+const kio = "114843644896346116" 
+const moo = "546812203697963036"
+const swr = "552871078008193063"
 
 function exec(msg, args) {
 
     if (args.length === 0) {
         error.NoArgsException(msg, args, "You must specify JavaScript code to evaluate.")
+        //msg.reply("You must specify JavaScript code to evaluate.")
+        return;
+    }
+
+    console.log(msg.author.id);
+    if (msg.author.id !== kio && msg.author.id !== moo && msg.author.id !== swr) {
+        error.InsufficientPermissionsException(msg, args, "This command is restricted to the bot's creator.")
+        return;
+        //msg.reply("You are not the bot's creator!");
     } else {
-        if (msg.author.id !== KIOID) {
-            error.InsufficientPermissionsException(msg, args, "This function is restricted to the bot's creator!")
-        } else {
-            try {
-                msg.channel.send(eval(msg.content.substring(5, msg.content.length)));
-                msg.delete();
-            } catch(e) {
-                error.GenericException(msg, args, "Eval failed.")
-                console.log(e)
-            };
+        try {
+            msg.channel.send(eval(msg.content.substring(5, msg.content.length)));
+            msg.delete();
+        } catch(e) {
+            msg.reply("Eval failed!");
+            console.log(e)
         };
-    };
+}   ;
 };
 
 module.exports = {exec: exec}
