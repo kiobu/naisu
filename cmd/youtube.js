ab27d10e444377bf42f795b37e0e5b1c29c649eb
kiobu <cdolson30@gmail.com>
2020-04-12 14:12:29 -0400

    Add --list parameter to youtube command.

--- cmd/youtube.js
+++ cmd/youtube.js
@@ -3,25 +3,44 @@ const error = require("../lib/error");
 const Discord = require("discord.js");
 
 const KEY = index.naisu.config.gapi;
+const EMBED = new Discord.RichEmbed();
 
 var search = require('youtube-search');
     var opts = {
-        maxResults: 10,
+        maxResults: 5,
         key: KEY,
     };
 
 exports.exec = function(msg, args) {
+    list = false;
+    for (i = 0; i < args.length; i++) {
+        if (args[i] === "--list") {
+            list = true;
+            args.splice(i, 1)
+            EMBED
+                .setColor('#ff0000')
+                .setTitle('Results')
+        }
+    }
     search(args.join(' '), opts, function(err, results) {
         if(err) return index.logger.error(`Could not create connection to YouTube API. Stack: ${$err.stack}`);
-       
         for (i = 0; i < results.length; i++) {
             if (results[i].kind == "youtube#video") {
-                msg.channel.send(results[i].link);
-                break;
+                if (!list) {
+                    msg.channel.send(results[i].link);
+                    break;
+                } else {
+                    EMBED.addField(results[i].title, results[i].link)
+                }
             } else {
                 continue;
             }
         }
+        if (list) {
+            msg.channel.send(EMBED);
+        }
+        
+        return;
     });
 }
 
