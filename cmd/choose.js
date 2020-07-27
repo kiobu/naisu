56dbfed53285f456a8b411228ea0cae723eba059
kiobu <cdolson30@gmail.com>
2020-07-27 13:41:02 -0400

    Fix argument parsing.

--- cmd/choose.js
+++ cmd/choose.js
@@ -2,15 +2,23 @@ const index = require('../index')
 const error = require('../lib/error')
 
 function exec(msg) {
-    let args = msg.content.substring(index.naisu.config.prefix.length, msg.length).split(',').map(x => x.trim());
-    args.shift()
+    let tmp = msg.content.substring(index.naisu.config.prefix.length, msg.length).split(' ').map(x => x.trim()); 
+    let args = [];
+
+    // Push the arguments to the args table.
+    for (let i = 1; i < tmp.length; i++) {
+        args.push(tmp[i].replace(',',''))
+    }
+
+    console.log(args)
     
     if (args.length === 0 || !args) {
         error.NoArgsException(msg, args, "You need to specify a list to choose from, separated by commas.")
         return;
     }
 
-    let choice = Math.floor(Math.random() * (args.length));
+    let choice = Math.floor(Math.random() * args.length);
+    console.log(choice)
     msg.channel.send(`I choose ...`)
         .then(message => {
             setTimeout(_ => { 
